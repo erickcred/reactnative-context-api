21cff24a31677157b1e2d4f81db8b1a00904d105
Erick Rick <erickcredjes@gmail.com>
2025-08-06 20:40:57 -0300

    Implementado processo de captura do parametro e navegação de retorno

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,16 +1,38 @@
-import { useNavigation } from "@react-navigation/native";
 import { Button, Text, View } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RouteProp } from "@react-navigation/native";
 
-export default function UserScreen() {
-  const navigation = useNavigation();
+import { RootStackParamList } from "../router/routes";
+
+
+type UserScreenProps = {
+  navigation: StackNavigationProp<RootStackParamList, "User">
+  route: RouteProp<RootStackParamList, "User">
+}
+
+export default function UserScreen({
+  navigation,
+  route
+}: UserScreenProps) {
+  const { userName } = route.params;
+  const navigationToHomeScreen = () => {
+    navigation.goBack();
+  }
 
   return (
     <View>
-      <Text>UserScreen</Text>
+      <Text
+        style={{
+          marginVertical: 10,
+          fontSize: 20,
+          fontWeight: 500,
+          textAlign: "center",
+        }}
+      >{userName}</Text>
 
       <Button
         title="Home"
-        onPress={() => navigation.goBack()}
+        onPress={navigationToHomeScreen}
       />
     </View>
   )
